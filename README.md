980f9b6ba1db1932636e72f42506f378c9d9e9c9
zdm <zdm@softvisio.net>
2019-12-19 20:09:12 +0200

    merged with other projects

--- README.md
+++ README.md
@@ -4,6 +4,32 @@
 yarn add @softvisio/core
 ```
 
+# API
+
+```
+import softvisioApi from '@softvisio/api';
+
+const api = new softvisioApi({
+    url: '//devel/api',
+    token: 'your-api-token',
+    version: 'v1',
+    onEvent: function (api, ev) { },
+    onRpc: function (api, cb, method, args) {
+        cb(200, data);
+        // cb([400, 'Error reason'], data);
+    },
+});
+
+api.call('class/method', arg1, argN, function (res) {
+    if ( res.isSuccess() ) {
+        console.log(res.data);
+    }
+    else {
+        console.log(res.status, res.reason);
+    }
+});
+``` 
+
 # CORO
 
 ```
---
+++ bin/cli.js
@@ -0,0 +1,128 @@
+#!/usr/bin/env node
+
+const net = require( "net" );
+const readline = require( "readline" );
+const eslintApi = require( "eslint/lib/cli-engine/cli-engine.js" );
+process.env.FORCE_COLOR = 0;
+const prettierApi = require( "prettier" );
+const terserApi = require( "terser" );
+
+var server = net.createServer( function ( socket ) {
+    var rl = readline.createInterface( socket, socket );
+
+    rl.on( "line", function ( line ) {
+        onCommand( line, function ( data ) {
+            socket.write( data + "\n" );
+        } );
+    } );
+} );
+
+server.listen( 55556, "127.0.0.1" );
+
+function onCommand ( data, req ) {
+    data = JSON.parse( data );
+
+    var res;
+
+    if ( data.command === "prettier" ) {
+        res = prettier( data );
+    }
+    else if ( data.command === "eslint" ) {
+        res = eslint( data );
+    }
+    else if ( data.command === "terser" ) {
+        res = terser( data );
+    }
+    else {
+        res = {
+            "status": 0,
+            "reason": "Invalid command",
+        };
+    }
+
+    req( JSON.stringify( res ) );
+}
+
+function eslint ( data ) {
+    let engine;
+
+    try {
+        engine = new eslintApi.CLIEngine( data.options );
+    }
+    catch ( error ) {
+        return {
+            "status": 0,
+            "reason": error.message,
+        };
+    }
+
+    const report = engine.executeOnText( data.data, data.path, true );
+
+    return {
+        "status": 1,
+        "result": report.results,
+    };
+}
+
+function prettier ( data ) {
+    var res;
+
+    var options;
+
+    try {
+        const config = prettierApi.resolveConfig.sync( data.options.filepath, {
+            "editorconfig": data.options.editorconfig,
+            "config": data.options.config,
+        } );
+
+        options = { ...data.options, ...config };
+    }
+    catch ( error ) {
+        return {
+            "status": 0,
+            "reason": error.message,
+        };
+    }
+
+    try {
+        res = prettierApi.format( data.data, options );
+    }
+    catch ( error ) {
+        return {
+            "status": 0,
+            "reason": error.message,
+        };
+    }
+
+    return {
+        "status": 1,
+        "result": res,
+    };
+}
+
+function terser ( data ) {
+    var res;
+
+    try {
+        res = terserApi.minify( data.data, data.options );
+    }
+    catch ( error ) {
+        return {
+            "status": 0,
+            "reason": error.message,
+        };
+    }
+
+    if ( res.error ) {
+        return {
+            "status": 0,
+            "reason": res.error.message,
+        };
+    }
+    else {
+        return {
+            "status": 1,
+            "result": res.code,
+        };
+    }
+}
