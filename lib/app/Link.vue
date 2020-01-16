429595614ebe5f8d6e15ac68d40ed23fdae0238c
zdm <zdm@softvisio.net>
2020-01-16 04:10:48 +0200

    standard tmpl

---
+++ lib/app/Link.vue
@@ -0,0 +1,28 @@
+<template>
+    <ext-panel @ready="ready"/>
+</template>
+
+<script>
+export default {
+    "name": "Link",
+
+    "props": ["text"],
+
+    "methods": {
+        ready ( e ) {
+            var cmp = ( this.cmp = e.detail.cmp );
+
+            var a = document.createElement( "a" );
+            a.textContent = this.text;
+            a.setAttribute( "href", "#" );
+            a.addEventListener( "click", this.click.bind( this ) );
+
+            cmp.setHtml( a );
+        },
+
+        click () {
+            this.$emit( "tap" );
+        },
+    },
+};
+</script>
