db36dc9c91816f1ed4998e57e23d3af4a5f27549
zdm <zdm@softvisio.net>
2021-05-14 20:05:01 +0300

    jest tests added

--- __tests__/001-stream.js
+++ __tests__/001-stream.js
@@ -1,34 +1,23 @@
 import { jest } from "@jest/globals";
 
-import Stream from "../lib/stream";
+jest.setTimeout( 2000 );
 
-jest.setTimeout( 5000 );
+import Stream from "../lib/stream";
 
 const buffer = "12-34--56--78-90";
 
 const TESTS = [
-    {
-        buffer,
-        "eol": "--",
-        "encoding": "utf8",
-        "maxSize": null,
-        "chunkSize": 1,
-        "line": "12-34",
-        "rest": "56--78-90",
-    },
-    {
-        buffer,
-        "eol": "---",
-        "encoding": "utf8",
-        "maxSize": null,
-        "chunkSize": 1,
-        "line": undefined,
-        "rest": buffer,
-    },
+    { buffer, "eol": "--", "encoding": "utf8", "maxSize": null, "chunkSize": 1, "line": "12-34", "rest": "5" },
+    { buffer, "eol": "--", "encoding": "utf8", "maxSize": null, "chunkSize": null, "line": "12-34", "rest": "56--78-90" },
+    { buffer, "eol": "---", "encoding": "utf8", "maxSize": null, "chunkSize": 1, "line": null, "rest": buffer },
 ];
 
-for ( const data of TESTS ) {
-    test( "1", async () => {
+const sleep = () => new Promise( resolve => setTimeout( resolve, 1 ) );
+
+for ( let n = 0; n < TESTS.length; n++ ) {
+    test( "test " + n, async () => {
+        const data = TESTS[n];
+
         const [line, rest] = await run( data );
 
         expect( line ).toBe( data.line );
@@ -47,10 +36,16 @@ async function run ( data ) {
             resolve( [line, rest + ""] );
         } );
 
-        for ( const buf of data.buffer.split( new RegExp( `(.{1,${data.chunkSize}})` ) ).filter( buf => buf !== "" ) ) {
-            stream.push( buf );
-        }
-
-        stream.push( null );
+        push( stream, data );
     } );
+}
+
+async function push ( stream, data ) {
+    for ( const buf of data.buffer.split( new RegExp( `(.{1,${data.chunkSize}})` ) ).filter( buf => buf !== "" ) ) {
+        stream.push( buf );
+
+        await sleep( 1 );
+    }
+
+    stream.push( null );
 }
