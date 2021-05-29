352164b2b7e73f9d735e715857cadad220cb8e69
zdm <zdm@softvisio.net>
2021-05-29 10:30:34 +0300

    app authentication fixed

---
+++ __tests__/benchmarks/await-function-call.js
@@ -0,0 +1,18 @@
+function test1 () {
+    return 1;
+}
+
+async function test2 () {
+    return 1;
+}
+
+const t = {
+    async async () {
+        for ( let n = 0; n < 10; n++ ) await test2();
+    },
+    sync () {
+        for ( let n = 0; n < 10; n++ ) test1();
+    },
+};
+
+await bench( "await", t, 10000000 );
