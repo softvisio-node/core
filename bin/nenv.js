177abc1ed60d5d43c3b24f9a963eb91d74773d77
zdm <zdm@softvisio.net>
2021-09-24 18:13:12 +0300

    feat: nenv script

---
+++ bin/nenv.js
@@ -0,0 +1,25 @@
+import child_process from "child_process";
+
+process.argv.shift();
+process.argv.shift();
+
+while ( process.argv.length ) {
+    const arg = process.argv.shift();
+
+    if ( arg == null || arg === "-" ) break;
+
+    const idx = arg.indexOf( "=" );
+
+    if ( idx < 1 ) process.exit( 2 );
+
+    process.env[arg.substr( 0, idx )] = arg.substring( idx + 1 );
+}
+
+if ( !process.argv.length ) process.exit( 2 );
+
+try {
+    child_process.execFileSync( process.argv.shift(), process.argv, { "stdio": "inherit", "shell": true } );
+}
+catch ( e ) {
+    process.exit( 2 );
+}
