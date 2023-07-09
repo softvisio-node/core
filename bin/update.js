ffb0222888c43e9b7edb79bff35f87651a62083a
zdm <zdm@softvisio.net>
2023-07-09 23:57:28 +0300

    feat: resoources refactored

--- bin/update.js
+++ bin/update.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import Cli from "#lib/cli";
-import resources from "#lib/resources/core";
+import PublicSuffixes from "#lib/resources/core1/public-suffixes";
 
 const CLI = {
     "title": "Update resources",
@@ -18,10 +18,15 @@ const CLI = {
 
 await Cli.parse( CLI );
 
-const res = await resources.update( { "build": process.cli.options.build } );
-
-if ( !res.ok ) {
-    console.log( `Resources update error: ` + res );
-
-    process.exit( 3 );
+if ( process.cli.options.build ) {
+    const res = await new PublicSuffixes().build();
+    if ( !res.ok ) process.exit( 1 );
 }
+
+// const res = await resources.update( { "build": process.cli.options.build } );
+
+// if ( !res.ok ) {
+//     console.log( `Resources update error: ` + res );
+
+//     process.exit( 3 );
+// }
