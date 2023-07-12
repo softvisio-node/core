391841a9c2dd17dfa7b1c8529e1e49b2d45a3d5c
zdm <zdm@softvisio.net>
2023-07-12 11:46:29 +0300

    fix: resource locations

--- bin/update.js
+++ bin/update.js
@@ -18,11 +18,11 @@ const CLI = {
 
 await Cli.parse( CLI );
 
-externalResources.add( "softvisio-node/core/resources/geolite2-country", import.meta.url );
-externalResources.add( "softvisio-node/core/resources/http", import.meta.url );
-externalResources.add( "softvisio-node/core/resources/public-suffixes", import.meta.url );
-externalResources.add( "softvisio-node/core/resources/subnets", import.meta.url );
-externalResources.add( "softvisio-node/core/resources/tld", import.meta.url );
+externalResources.add( "softvisio-node/core/resources/geolite2-country" );
+externalResources.add( "softvisio-node/core/resources/http" );
+externalResources.add( "softvisio-node/core/resources/public-suffixes" );
+externalResources.add( "softvisio-node/core/resources/subnets" );
+externalResources.add( "softvisio-node/core/resources/tld" );
 
 const res = await externalResources.update( {
     "remote": true,
