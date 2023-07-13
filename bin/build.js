ad492804e5d37021f651b16783f5513f42944287
zdm <zdm@softvisio.net>
2023-07-13 13:07:49 +0300

    fix: subnets updater

--- bin/build.js
+++ bin/build.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 import Cli from "#lib/cli";
-import Geolite2Country from "#lib/external-resources/geolite2-country";
-import Http from "#lib/external-resources/http";
-import PublicSuffixes from "#lib/external-resources/public-suffixes";
-import Subnets from "#lib/external-resources/subnets";
-import Tld from "#lib/external-resources/tld";
+import Geolite2Country from "#lib/external-resources/core/geolite2-country";
+import Http from "#lib/external-resources/core/http";
+import PublicSuffixes from "#lib/external-resources/core/public-suffixes";
+import Subnets from "#lib/external-resources/core/subnets";
+import Tld from "#lib/external-resources/core/tld";
 
 const CLI = {
     "title": "Update resources",
