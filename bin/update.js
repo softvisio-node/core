6b88f4a8bf7deb0c3d7a036133a4aa78a86044f6
zdm <zdm@softvisio.net>
2023-07-13 07:46:53 +0300

    fix: find root

--- bin/update.js
+++ bin/update.js
@@ -3,8 +3,6 @@
 import Cli from "#lib/cli";
 import externalResources from "#lib/external-resources";
 
-if ( process.env.DOWNLOAD_EXTERNAL_RESOURCES === "false" ) process.exit( 0 );
-
 const CLI = {
     "title": "Update resources",
     "options": {
