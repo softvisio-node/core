7d08563b3b4749a0bc9ed5c9748048ffe9789d4a
zdm <zdm@softvisio.net>
2023-07-13 16:00:48 +0300

    fix: external resource index

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import Cli from "#lib/cli";
+import ExternalResourceBuilder from "#lib/external-resource-builder";
 import Geolite2Country from "#lib/external-resources/geolite2-country";
 import Http from "#lib/external-resources/http";
 import PublicSuffixes from "#lib/external-resources/public-suffixes";
@@ -22,19 +23,17 @@ const CLI = {
 
 await Cli.parse( CLI );
 
-var res;
+const res = await ExternalResourceBuilder.build(
+    [
 
-res = await new Geolite2Country().build( { "force": process.cli.options.force } );
-if ( !res.ok ) process.exit( 1 );
+        //
+        Geolite2Country,
+        Http,
+        PublicSuffixes,
+        Subnets,
+        Tld,
+    ],
+    { "force": process.cli.options.force }
+);
 
-res = await new Http().build( { "force": process.cli.options.force } );
-if ( !res.ok ) process.exit( 1 );
-
-res = await new PublicSuffixes().build( { "force": process.cli.options.force } );
-if ( !res.ok ) process.exit( 1 );
-
-res = await new Subnets().build( { "force": process.cli.options.force } );
-if ( !res.ok ) process.exit( 1 );
-
-res = await new Tld().build( { "force": process.cli.options.force } );
 if ( !res.ok ) process.exit( 1 );
