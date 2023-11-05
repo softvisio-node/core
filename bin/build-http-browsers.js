5d386f267e3bd2177c3a7102d06661c2236ea634
zdm <zdm@softvisio.net>
2023-11-05 15:13:22 +0200

    fix: resources

---
+++ bin/build-http-browsers.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import playwright from "@softvisio/playwright";
+import yaml from "#lib/yaml";
+
+const browser = await playwright.chromium.launch( {
+
+    // "headless": true,
+} );
+
+const page = await browser.newPage( {} );
+
+const data = {
+    "userAgent": null,
+};
+
+var headers = await getHeaders( "http://httpbin.org/headers" );
+parseHeaders( "http", headers );
+
+headers = await getHeaders( "https://httpbin.org/headers" );
+parseHeaders( "https", headers );
+
+console.log( yaml.stringify( data ) );
+
+process.exit();
+
+async function getHeaders ( url ) {
+    await page.goto( url );
+
+    const content = await page.content();
+
+    // console.log( content );
+
+    const match = content.match( /{\s+"headers":(.+?\})/s );
+
+    const json = match[1].trim();
+
+    const data = JSON.parse( json );
+
+    return data;
+}
+
+function parseHeaders ( type, headers ) {
+    data[type] = {};
+
+    for ( const [header, value] of Object.entries( headers ) ) {
+        const name = header.toLowerCase();
+
+        if ( name === "user-agent" ) {
+            data["userAgent"] = value;
+        }
+        else if ( name === "dnt" ) {
+            data[type][header] = value;
+        }
+        else if ( name === "accept" ) {
+            data[type][header] = value;
+        }
+        else if ( name === "accept-language" ) {
+            data[type][header] = value;
+        }
+        else if ( name.startsWith( "sec-" ) ) {
+            data[type][header] = value;
+        }
+    }
+}
