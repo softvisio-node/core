c44d3a6db8b485c230ff69075f92c234beca864f
zdm <zdm@softvisio.net>
2023-12-02 14:47:20 +0200

    fix: geoip

--- bin/build.js
+++ bin/build.js
@@ -2,9 +2,9 @@
 
 import Cli from "#lib/cli";
 import ExternalResourceBuilder from "#lib/external-resource-builder";
-import Geolite2Country from "#lib/external-resources/geolite2-country";
-import Geolite2City from "#lib/external-resources/geolite2-city";
 import Geolite2Asn from "#lib/external-resources/geolite2-asn";
+import Geolite2City from "#lib/external-resources/geolite2-city";
+import Geolite2Country from "#lib/external-resources/geolite2-country";
 import Http from "#lib/external-resources/http";
 import PublicSuffixes from "#lib/external-resources/public-suffixes";
 import Subnets from "#lib/external-resources/subnets";
@@ -29,9 +29,9 @@ const res = await ExternalResourceBuilder.build(
     [
 
         //
-        Geolite2Country,
-        Geolite2City,
         Geolite2Asn,
+        Geolite2City,
+        Geolite2Country,
         Http,
         PublicSuffixes,
         Subnets,
