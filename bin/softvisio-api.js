35df476a1237efdb2ab8f1e180448c5588c53c27
zdm <zdm@softvisio.net>
2024-07-28 04:06:11 +0300

    feat: api cli

---
+++ bin/softvisio-api.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+import Cli from "#lib/cli";
+import Api from "#lib/api";
+
+const CLI = {
+    "title": "Core API client",
+    "globalOptions": {
+        "url": {
+            "description": "API url",
+
+            // "required": true,
+            "schema": {
+                "type": "string",
+            },
+        },
+        "default-version": {
+            "short": "v",
+            "description": "default API version",
+            "default": 1,
+            "schema": {
+                "type": "integer",
+            },
+        },
+        "token": {
+            "description": "API token",
+            "schema": {
+                "type": "string",
+            },
+        },
+    },
+    "commands": {
+        "call": {
+            "title": "make API call",
+            "arguments": {
+                "method": {
+                    "description": "API method",
+                    "required": true,
+                    "schema": {
+                        "type": "string",
+                    },
+                },
+            },
+        },
+    },
+};
+
+await Cli.parse( CLI );
+
+const url = new URL( process.cli.options.url, "http://" );
+
+const api = new Api( url, {
+    "token": process.cli.options.token,
+} );
+
+const res = await api.call( process.cli.arguments.method );
+
+console.log( res );
