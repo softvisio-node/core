59c05ac0765d1bde659a377545a6f897dddc190f
zdm <zdm@softvisio.net>
2024-07-30 10:51:37 +0300

    fix: tests

---
+++ _tests/benchmarks/try-catch.benchmark.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import benchmark from "#lib/benchmark";
+
+function test1 () {
+    return 1;
+}
+
+async function test2 () {
+    return 1;
+}
+
+const tests = {
+    async [ "async" ] () {
+        for ( let n = 0; n < 10; n++ ) await test2();
+    },
+
+    sync () {
+        for ( let n = 0; n < 10; n++ ) test1();
+    },
+};
+
+await benchmark( "Try / catch speed test", tests, {
+    "iterations": 1_000_000,
+    "seconds": 3,
+    "threads": 1,
+} );
