194247a1cf05a150aaa38f2ec6e03cf6346dbe4d
zdm <zdm@softvisio.net>
2024-07-30 11:01:41 +0300

    fix: tests

--- _tests/benchmarks/await.benchmark.js
+++ _tests/benchmarks/await.benchmark.js
@@ -2,26 +2,18 @@
 
 import benchmark from "#lib/benchmark";
 
-function test1 () {
-    return 1;
-}
-
-async function test2 () {
-    return 1;
-}
-
 const tests = {
     async [ "async" ] () {
-        for ( let n = 0; n < 10; n++ ) await test2();
+        return 1;
     },
 
     sync () {
-        for ( let n = 0; n < 10; n++ ) test1();
+        return 1;
     },
 };
 
 await benchmark( "Await speed test", tests, {
-    "iterations": 1_000_000,
+    "iterations": null,
     "seconds": 3,
     "threads": 1,
 } );
