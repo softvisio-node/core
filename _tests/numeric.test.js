96c0224fa80debc25772576969e9eadaf5f6f0c8
zdm <zdm@softvisio.net>
2024-09-28 20:43:58 +0300

    fix: numeric props

--- _tests/numeric.test.js
+++ _tests/numeric.test.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { suite, test } from "node:test";
+import { strictEqual } from "node:assert";
 
 import Numeric from "#lib/numeric";
 
@@ -36,6 +37,90 @@ function generateRandomIntegers ( { min, max, num, crypto, threshold = 0.03 } ) 
 }
 
 suite( "numeric", () => {
+    suite( "precision", () => {
+        const tests = [
+
+            // precision
+            {
+                "constructor": [ "123", { "precision": 3 } ],
+                "result": "123",
+            },
+            {
+                "constructor": [ "123", { "precision": 2 } ],
+                "result": null,
+            },
+            {
+                "constructor": [ "123", { "precision": 4 } ],
+                "result": "123",
+            },
+            {
+                "constructor": [ "123.123", { "precision": 2 } ],
+                "result": null,
+            },
+            {
+                "constructor": [ "123.123", { "precision": 3 } ],
+                "result": "123",
+            },
+            {
+                "constructor": [ "123.123", { "precision": 4 } ],
+                "result": "123.1",
+            },
+            {
+                "constructor": [ "123.123", { "precision": 5 } ],
+                "result": "123.12",
+            },
+            {
+                "constructor": [ "123.199", { "precision": 5 } ],
+                "result": "123.2",
+            },
+            {
+                "constructor": [ "123.199", { "precision": 6 } ],
+                "result": "123.199",
+            },
+            {
+                "constructor": [ "123.999", { "precision": 4 } ],
+                "result": "124",
+            },
+            {
+                "constructor": [ "999.999", { "precision": 3 } ],
+                "result": null,
+            },
+            {
+                "constructor": [ "999.999", { "precision": 4 } ],
+                "result": "1000",
+            },
+
+            // scale
+            {
+                "constructor": [ "123.123456", { "precision": 4, "scale": 1 } ],
+                "result": "123.1",
+            },
+            {
+                "constructor": [ "123.123456", { "precision": 4, "scale": 0 } ],
+                "result": "123",
+            },
+            {
+                "constructor": [ "123.123456", { "precision": 4, "scale": 2 } ],
+                "result": null,
+            },
+        ];
+
+        for ( let n = 0; n < tests.length; n++ ) {
+            test( n + "", () => {
+                try {
+                    const numeric = Numeric( ...tests[ n ].constructor );
+
+                    strictEqual( numeric.toString(), tests[ n ].result );
+                }
+                catch ( e ) {
+                    if ( tests[ n ].result != null ) {
+                        throw e;
+                    }
+                }
+            } );
+        }
+    } );
+
     suite( "random-integers-distribution", () => {
         test( "1", () => {
             generateRandomIntegers( {
