205bb46e858a9a74a22c6aff75a2b57cea735a56
zdm <zdm@softvisio.net>
2025-01-06 10:03:54 +0200

    chore: add chrome headless shell resource

--- bin/build.js
+++ bin/build.js
@@ -3,13 +3,13 @@
 import Cli from "#lib/cli";
 import ExternalResourceBuilder from "#lib/external-resource-builder";
 import Certificates from "#lib/external-resources/certificates";
-import ChromeHeadlessShellStableDarwinX64 from "#lib/external-resources/chrome-headless-shell-stable-darwin-x64";
-import ChromeHeadlessShellStableLinuxX64 from "#lib/external-resources/chrome-headless-shell-stable-linux-x64";
-import ChromeHeadlessShellStableWin32X64 from "#lib/external-resources/chrome-headless-shell-stable-win32-x64";
 import Ffmpeg from "#lib/external-resources/ffmpeg-win32";
 import Geolite2Asn from "#lib/external-resources/geolite2-asn";
 import Geolite2City from "#lib/external-resources/geolite2-city";
 import Geolite2Country from "#lib/external-resources/geolite2-country";
+import GoogleChromeHeadlessShellStableDarwinX64 from "#lib/external-resources/google-chrome-headless-shell-stable-darwin-x64";
+import GoogleChromeHeadlessShellStableLinuxX64 from "#lib/external-resources/google-chrome-headless-shell-stable-linux-x64";
+import GoogleChromeHeadlessShellStableWin32X64 from "#lib/external-resources/google-chrome-headless-shell-stable-win32-x64";
 import GoogleGeotargets from "#lib/external-resources/google-geotargets";
 import Http from "#lib/external-resources/http";
 import Mime from "#lib/external-resources/mime";
@@ -38,13 +38,13 @@ const res = await ExternalResourceBuilder.build(
 
         //
         Certificates,
-        ChromeHeadlessShellStableDarwinX64,
-        ChromeHeadlessShellStableLinuxX64,
-        ChromeHeadlessShellStableWin32X64,
         Ffmpeg,
         Geolite2Asn,
         Geolite2City,
         Geolite2Country,
+        GoogleChromeHeadlessShellStableDarwinX64,
+        GoogleChromeHeadlessShellStableLinuxX64,
+        GoogleChromeHeadlessShellStableWin32X64,
         GoogleGeotargets,
         Http,
         Mime,
