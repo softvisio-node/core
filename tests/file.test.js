2b1eda855c5d677062d0cfcc8f60221b11c39a57
zdm <zdm@softvisio.net>
2025-07-08 08:24:29 +0300

    refactor: update file stream

---
+++ tests/file.test.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import "#lib/stream";
+import { strictEqual } from "node:assert";
+import fs from "node:fs";
+import { suite, test } from "node:test";
+import { TmpFile } from "#lib/tmp";
+
+suite( "file", () => {
+    suite( "stream", () => {
+        const file = new TmpFile(),
+            content = "0123456789";
+
+        fs.writeFileSync( file.path, content );
+
+        const TESTS = [
+
+            //
+            { "start": undefined, "end": undefined },
+            { "start": null, "end": null },
+            { "start": null, "end": 80 },
+            { "start": 3, "end": null },
+            { "start": 0, "end": 0 },
+            { "start": 0, "end": 1 },
+            { "start": 0, "end": 3 },
+            { "start": 0, "end": -3 },
+            { "start": -7, "end": -3 },
+            { "start": -7, "end": -8 },
+        ];
+
+        for ( let n = 0; n < TESTS.length; n++ ) {
+            test( n + "", async () => {
+                const stream = file.stream( {
+                        "start": TESTS[ n ].start,
+                        "end": TESTS[ n ].end,
+                    } ),
+                    text = await stream.text();
+
+                strictEqual( content.slice( TESTS[ n ].start, TESTS[ n ].end ).length, stream.size );
+                strictEqual( content.slice( TESTS[ n ].start, TESTS[ n ].end ), text );
+            } );
+        }
+    } );
+} );
