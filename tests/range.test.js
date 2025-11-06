d3c5e42d0eca395c2dbe923a3b3645fcd4622b7f
zdm <zdm@softvisio.net>
2025-11-06 18:17:04 +0200

    feat: add ranges

--- tests/range.test.js
+++ tests/range.test.js
@@ -6,7 +6,52 @@ import Range from "#lib/range";
 
 suite( "range", () => {
     suite( "calculate-readable-stream-range", () => {
-        const tests = [ { "start": null, "end": null, "length": null, "contentLength": null, "result": {} } ];
+        const tests = [
+            {
+                "start": null,
+                "end": null,
+                "length": null,
+                "contentLength": null,
+                "result": {
+                    "start": 0,
+                    "end": undefined,
+                    "length": undefined,
+                },
+            },
+            {
+                "start": 10,
+                "end": null,
+                "length": null,
+                "contentLength": null,
+                "result": {
+                    "start": 10,
+                    "end": undefined,
+                    "length": undefined,
+                },
+            },
+            {
+                "start": 10,
+                "end": 10,
+                "length": null,
+                "contentLength": null,
+                "result": {
+                    "start": 10,
+                    "end": -1,
+                    "length": 0,
+                },
+            },
+            {
+                "start": 10,
+                "end": 20,
+                "length": null,
+                "contentLength": null,
+                "result": {
+                    "start": 10,
+                    "end": 19,
+                    "length": undefined,
+                },
+            },
+        ];
 
         for ( let n = 0; n < tests.length; n++ ) {
             test( `${ n }`, () => {
